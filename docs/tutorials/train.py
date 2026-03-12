70d96b5fd4d95779f5e5e7047bc3e419b1874d49
Gustavo Arango <gustavo1@vt.edu>
2026-03-12 19:10:35 -0400

    Add training pipeline for BERT model
    Implement training pipeline for BERT model using clinical transformer with logging and checkpointing.

---
+++ docs/tutorials/train.py
@@ -0,0 +1,109 @@
+import os
+os.environ["HDF5_USE_FILE_LOCKING"] = "FALSE"
+
+import sys
+import yaml 
+import anndata
+import lightning
+import torch
+from transformers.models.bert.modeling_bert import BertConfig
+from clinical_transformer.pt.training.vnBERT.dataset import MaskedTokenDatasetFromAnnData as MaskedTokenDataset
+from clinical_transformer.pt.training.vnBERT.modeling import LightningTrainerModel
+from clinical_transformer.pt.training import Config
+
+import pickle
+from lightning.pytorch import Trainer
+
+from lightning.pytorch.loggers import CSVLogger
+from lightning.pytorch.callbacks import ModelCheckpoint
+from lightning.pytorch import Trainer, seed_everything
+from torch.utils.data import DataLoader
+import logging 
+
+from lightning.pytorch.strategies import DeepSpeedStrategy
+
+def pipeline():
+    """
+    Pipeline for training BERT model using clinical transformer.
+    """
+    config_file = sys.argv[1]
+    config_ = yaml.safe_load(open(config_file, 'r'))
+    config = Config(config_)
+
+    # Global configs 
+    torch.backends.cuda.enable_flash_sdp(enabled=config.model.enable_flash_attention)
+    seed_everything(config.experiment.seed, workers=True)
+    torch.set_float32_matmul_precision(config.experiment.set_float32_matmul_precision)
+    config.experiment.output_dir = f'{config.experiment.save_dir}/{config.experiment.name}/version_{config.experiment.version}/'
+    os.makedirs(config.experiment.output_dir, exist_ok=True)
+
+    yaml.dump(config_, open(f'{config.experiment.output_dir}/experiment_{config_file.split("/")[-1]}', 'w'))
+    
+    # Loggers
+    csv_logger = CSVLogger(
+        save_dir=config.experiment.save_dir, 
+        name=config.experiment.name, 
+        version=config.experiment.version
+    )
+
+    checkpoint_callback = ModelCheckpoint(
+        monitor="epoch",  # You can monitor a specific metric, but for saving after each epoch, we use "epoch"
+        dirpath=f"{config.experiment.output_dir}/models/",  # Directory to save the models
+        filename="{epoch}",  # Filename format for saving the model
+        save_top_k=-1,  # Save all models (otherwise it saves only the best `k` models)
+        save_weights_only=False,  # Save only the model weights (no optimizer states, etc.)
+        every_n_epochs=config.trainer.save_every_n_epochs  # Save every epoch
+    )
+
+    train_dataset = MaskedTokenDataset(
+        anndata_path = config.dataset.input_file,
+        # prior_path = config.dataset.priors_file,
+        context_window=config.dataset.context_window,
+        mask_prob=config.dataset.masking_fraction,
+        filter_zeros=config.dataset.filter_zeros,
+    )
+    train_dataloader = DataLoader(
+        train_dataset, 
+        batch_size=config.dataset.batch_size, 
+        shuffle=config.dataset.shuffle, 
+        num_workers=config.dataset.num_workers
+    )
+
+    model_config = BertConfig(
+        **config.model.__dict__,
+    )
+
+    model = LightningTrainerModel(model_config)
+    model_config.to_json_file(f'{config.experiment.output_dir}/model_config.json')
+    
+    if config.trainer.strategy['name'] == 'deepspeed':
+        ds_strategy = DeepSpeedStrategy(
+            **config.trainer.strategy.params.__dict__
+        )
+    else:
+        ds_stragety = config.trainer.strategy['name']
+    
+    # Trainer
+    trainer = Trainer(
+        log_every_n_steps=config.trainer.log_every_n_steps,
+        deterministic=config.trainer.deterministic,
+        devices=config.trainer.devices,
+        accelerator=config.trainer.accelerator,
+        strategy=ds_strategy,
+        max_epochs=config.trainer.epochs,
+        precision=config.trainer.precision,
+        accumulate_grad_batches=config.trainer.accumulate_grad_batches,
+        reload_dataloaders_every_n_epochs=config.trainer.reload_dataloaders_every_n_epochs,
+        logger=csv_logger,
+        callbacks=[checkpoint_callback],
+        num_nodes=config.trainer.num_nodes,
+    )
+
+    trainer.fit(
+        model=model, 
+        train_dataloaders=train_dataloader,
+        ckpt_path= config.trainer.from_checkpoint
+    )
+
+if __name__ == "__main__":
+    pipeline()
